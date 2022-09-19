1a2f1a4c01e3db8f8f0d9275119aa3cb75e3bf34
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-19 12:50:10 +0530

    first commit

---
+++ fetch_data.php
@@ -0,0 +1,25 @@
+<?php
+$db_hostname = "127.0.0.1"; 
+$db_username = "root"; 
+$db_password = ""; 
+$db_name = "test";
+
+$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name); 
+if (!$conn) {
+   echo "Connection failed: " . mysqli_connect_error(); 
+   exit;
+}
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql); 
+if (!$result) {
+     echo "Error: ". mysqli_error($conn);
+
+}
+while ($row=mysqli_fetch_assoc($result)) {
+ echo $row['name'] . "<br/>";
+
+} 
+  mysqli_close($conn);
+
+?>
